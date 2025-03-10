4ee9949daa3083bc9afad44e40ef02e9fb60ec03
Ozan Orhan <ozanorhan60@icloud.com>
2025-03-10 18:51:13 +0100

    UI/UX improvements: Navbar, Footer, Icon section & spacing optimized

--- src/app/main/hero-section/hero-section.component.ts
+++ src/app/main/hero-section/hero-section.component.ts
@@ -3,11 +3,12 @@ import { BannerComponent } from './banner/banner.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../../services/translate.service';
 import { ScrollService } from '../../services/scroll.service';
+import { HeaderComponent } from "../../shared/header/header.component";
 
 @Component({
   selector: 'app-hero-section',
   standalone: true, 
-  imports: [ BannerComponent, TranslateModule], 
+  imports: [BannerComponent, TranslateModule, HeaderComponent], 
   templateUrl: './hero-section.component.html',
   styleUrls: ['./hero-section.component.scss']
 })
@@ -19,6 +20,8 @@ export class HeroSectionComponent {
   navigateWithOffset(targetId: string, offset: number = 100) {
     this.scrollService.navigateWithOffset(targetId, offset);
   }
+
+  
 }
 
 
